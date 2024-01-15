f815580415f7d5ca0cea15c38567a95f4513ab94
polo <polo-ru@yandex.ru>
2024-01-15 16:38:32 +0300

    edits

---
+++ src/main/java/dev/lesechko/proselyte/model/File.java
@@ -0,0 +1,33 @@
+package dev.lesechko.proselyte.model;
+
+public class File {
+    private Integer id;
+    private String name;
+    private String filePath;
+
+    public File() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
